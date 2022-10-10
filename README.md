ad03ac85a4ca8d39e42d17a20aaf51a24312f6a8
Abhiram Kadiyala <akadiy2@gmail.com>
2022-10-10 18:27:04 -0500

    Unit tests

---
+++ README.md
@@ -0,0 +1,26 @@
+#Installation
+
+Prerequisites: Gradle and Java 8 or above
+
+1. Install MongoDB
+    - Follow the instructions to install MongoDB Community Edition (or a version of your choosing) from the official webpage: https://www.mongodb.com/docs/manual/administration/install-community/
+    - Set the path variable to the installation directory of mongodb/bin on the system explorer (for Windows users)
+    
+2. Run the application locally
+   - Navigate to the root of "EventService"
+   - Run "./gradlew build" or update the project's dependencies using the respective IDE's gradle perspective.
+   - Run EventService application by right clicking on the file and select "Run Application"
+
+#Usage
+
+1.  On a rest client (such as postman), add an event object to the following url: "http://localhost:8102/events"
+```{"id": 1, "name": "Event1", "description": "Description of event1"}```
+
+
+- curl --location --request POST 'localhost:8102/events' \
+   --header 'Content-Type: application/json' \
+   --data-raw '{"id": 1, "name": "Event1", "description": "Description of event1"}'
+   
+2. To view data in mongodb (events) run GET on "http://localhost:8102/events"
+
+       
--- src/test/java/com/projects/EventService/EventServiceApplicationTests.java
+++ src/test/java/com/projects/EventService/EventServiceApplicationTests.java
@@ -1,13 +1,74 @@
 package com.projects.EventService;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@DisplayName("EventService Tests")
 class EventServiceApplicationTests {
 
+	protected static MockMvc mvc;
+
+	private static final Event event = new Event("1", "Event1", "Description of event1");
+
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+	private ObjectMapper mapper = new ObjectMapper();
+
+
+	public void setUp() {
+		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+		mapper = new ObjectMapper();
+	}
+
+	protected <T> T mapFromJson(String json, Class<T> clazz)
+			throws Exception {
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.readValue(json, clazz);
+	}
+
 	@Test
-	void contextLoads() {
+	public void createEvent() throws Exception {
+		setUp();
+		String uri = "/events";
+		Event event = new Event();
+		event.setId("1");
+		event.setDescription("Description of event1");
+		event.setName("Event1");
+
+		String content = mapper.writeValueAsString(event);
+
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+				.contentType(MediaType.APPLICATION_JSON_VALUE).content(content)).andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+	}
+
+	private static Stream<Event> events() {
+		return Stream.of(new Event("1", "Event1", "Description of event1"));
 	}
 
 }
